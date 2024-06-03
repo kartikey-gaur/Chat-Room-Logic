abfa667138c1d52889681cac2725d62716ed0d4e
kartikey-gaur <kartikeygaur08.1@gmail.com>
2024-06-03 20:13:48 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,66 +1,132 @@
-import WebSocket, { WebSocketServer } from "ws";
-import http from 'http';
-const port = 8080;
+/* import { useEffect, useState } from 'react'
+import './App.css'
 
-const server = http.createServer(function (request: any, response: any) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.end("hi there");
-});
+function App() {
+  //the <> contains the type of state can be out of many
+  const [socket, setSocket] = useState<null | WebSocket>(null) ;
+  //scoket above can hold a variable of the type webSocekt Connection
+  const [message, setMessages] = useState("") ; //re renderer helping for the interface/front end
+  const[reply, setReply] = useState("") ; // what are replaying same task as the message state variable
 
-//Websocket logic starts
+  useEffect(()=>{
+    //connection gets created here
+    const socketIn = new WebSocket("ws://localhost:8080") ;
+    //the above line of code makes a client socket and it takes the server as its arguemnet
+    socketIn.onopen = () =>{ // this event is called when the connection is successfully estd.
+      console.log("the server is connected") ;
+      setSocket(socketIn) ;
+      //resetting the socket connection to the websocket server here
+    }
+    socketIn.onmessage = (message) => {//this is called when the client receives a  message from the server.
+      console.log(`message has been received ${message.data}`) ;
+      setMessages(message.data) ;
+    }
 
-const wss = new WebSocketServer({ server }); //WebSocketServer here is instanctized
+    return () =>{
+      socketIn.close() ;
+    }
+  }, []);
 
-let usercount = 0;
+  if(!socket)
+    {
+      return (
+        <div>
+          <h1>
+            Loading....
+          </h1>
+        </div>
+      )
+    }
 
-wss.on("connection", function connectionHere(socket) { // socket here represents the new connection between client and server
-    socket.on("error", console.error); //console.log(error) is same as console.error
+    return (
+      <div>
+        <input style= {{height: 40, width: 500}}type="text" onChange={(res)=>{
+          const currMessage = res.target.value ;
+          setReply(currMessage) ;
+        }}/>
+        <br /><br />
+        <button onClick={()=>{
+           if (socket) {
+            socket.send(reply);
+        }
+        }}>Send me the message</button>
+        <h2>
+          {message}
+        </h2>
+      </div>
+    )
 
-    //the below method is that when we receive data from the clients
-    //then, for each connected client, we must send something to the clients
-    //the below code receives messages from client
+}
 
-    socket.on("message", function messageHere(data, isBinary) {
-        //console.log(data);
-        wss.clients.forEach(function each(client) {
-            if (client !== socket && client.readyState === WebSocket.OPEN) {
-                client.send("Friend: " + data,{ binary: false });
-            }
-        });
-    });
-    //console.log('user connnected: ', ++usercount);
-    //the below line sends the code to the client
-    socket.send("Send messages to start chatting");
-});
+export default App
+ */
 
-//websocket logic ends
-server.listen(port, function () {
-    console.log(' Server is listening on port 8080');
-});
+import { useEffect, useState } from 'react';
+import './App.css';
 
-//client !== socket : additional condition to not thte data sen tby the user itself to the user back
+function App() {
+  const [socket, setSocket] = useState<null | WebSocket>(null) ;
+  const [messages, setMessages] = useState([""]);
+  const [reply, setReply] = useState('');
+
+  useEffect(() => {
+    const socketIn = new WebSocket('ws://localhost:8080');
+
+    socketIn.onopen = () => {
+      console.log('Connected to server');
+      setSocket(socketIn);
+    };
+
+    socketIn.onmessage = (event) => {
+      const incomingMessage = event.data;
+      //console.log(`Message received: ${incomingMessage}`);
+      setMessages((prevMessages) => [...prevMessages, incomingMessage]);
+    };
 
 
-//same code using Express.JS
+    return () => {
+      socketIn.close();
+    };
+  }, []);
 
-/* import express from 'express'
-import WebSocket, { WebSocketServer } from 'ws'
+  const sendMessage = () => {
+    if (socket) {
+      socket.send(reply);
+      //console.log(`Message sent: ${reply}`);
+      setMessages(prevMessages => [...prevMessages, "Me: " + reply]);
+      setReply('');
+    }
+  };
 
-const app = express()
-const httpServer = app.listen(8080)
+  if (!socket) {
+    return (
+      <div>
+        <h1>Loading....</h1>
+      </div>
+    );
+  }
 
-const wss = new WebSocketServer({ server: httpServer });
+  return (
+    <div>
+      <input
+        style={{ height: 40, width: 500 }}
+        type="text"
+        value={reply}
+        onChange={(res) => {
+          const currMessage = res.target.value;
+          setReply(currMessage);
+        }}
+      />
+      <br />
+      <br />
+      <button onClick={sendMessage}>Send me the message</button>
+      <h2>
+        {messages.map((msg, index) => (
+          <div key={index}>{msg}</div>
+        ))}
+      </h2>
+    </div>
+  );
+}
 
-wss.on('connection', (ws)=> {
-  ws.on('error', console.error);
-
-  ws.on('message', (data, isBinary) =>{
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data, { binary: false });
-      }
-    });
-  });
-
-  ws.send('Hello! Message From Server!!');
-}); */
+export default App;
